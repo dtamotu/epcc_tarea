b3f5783a7b22c6415070282672b68568ad9587a6
dtamotu <104528081+dtamotu@users.noreply.github.com>
2023-07-01 20:16:26 -0500

    reparando strassen

--- 3/strassen.cpp
+++ 3/strassen.cpp
@@ -6,38 +6,38 @@ using namespace std;
 vector<vector<int>> sum(const vector<vector<int>>& A, const vector<vector<int>>& B) {
     int n = A.size();
     vector<vector<int>> result(n, vector<int>(n, 0));
-    
+
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             result[i][j] = A[i][j] + B[i][j];
         }
     }
-    
+
     return result;
 }
 
 vector<vector<int>> subtract(const vector<vector<int>>& A, const vector<vector<int>>& B) {
     int n = A.size();
     vector<vector<int>> result(n, vector<int>(n, 0));
-    
+
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             result[i][j] = A[i][j] - B[i][j];
         }
     }
-    
+
     return result;
 }
 void split(const vector<vector<int>>& A, vector<vector<int>>& A11, vector<vector<int>>& A12,
-           vector<vector<int>>& A21, vector<vector<int>>& A22) {
+    vector<vector<int>>& A21, vector<vector<int>>& A22) {
     int n = A.size();
     int m = n / 2;
-    
+
     A11.resize(m, vector<int>(m, 0));
     A12.resize(m, vector<int>(m, 0));
     A21.resize(m, vector<int>(m, 0));
     A22.resize(m, vector<int>(m, 0));
-    
+
     for (int i = 0; i < m; ++i) {
         for (int j = 0; j < m; ++j) {
             A11[i][j] = A[i][j];
@@ -49,11 +49,11 @@ void split(const vector<vector<int>>& A, vector<vector<int>>& A11, vector<vector
 }
 
 vector<vector<int>> combine(const vector<vector<int>>& C11, const vector<vector<int>>& C12,
-                            const vector<vector<int>>& C21, const vector<vector<int>>& C22) {
+    const vector<vector<int>>& C21, const vector<vector<int>>& C22) {
     int n = C11.size();
     int m = 2 * n;
     vector<vector<int>> result(m, vector<int>(m, 0));
-    
+
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             result[i][j] = C11[i][j];
@@ -62,22 +62,22 @@ vector<vector<int>> combine(const vector<vector<int>>& C11, const vector<vector<
             result[i + n][j + n] = C22[i][j];
         }
     }
-    
+
     return result;
 }
 
 vector<vector<int>> strassen(const vector<vector<int>>& A, const vector<vector<int>>& B) {
     int n = A.size();
-    
+
     if (n == 1) {
-        return {{A[0][0] * B[0][0]}};
+        return { {A[0][0] * B[0][0]} };
     }
-    
+
     vector<vector<int>> A11, A12, A21, A22;
     vector<vector<int>> B11, B12, B21, B22;
     split(A, A11, A12, A21, A22);
     split(B, B11, B12, B21, B22);
-    
+
     vector<vector<int>> P1 = strassen(sum(A11, A22), sum(B11, B22));
     vector<vector<int>> P2 = strassen(sum(A21, A22), B11);
     vector<vector<int>> P3 = strassen(A11, subtract(B12, B22));
@@ -85,18 +85,18 @@ vector<vector<int>> strassen(const vector<vector<int>>& A, const vector<vector<i
     vector<vector<int>> P5 = strassen(sum(A11, A12), B22);
     vector<vector<int>> P6 = strassen(subtract(A21, A11), sum(B11, B12));
     vector<vector<int>> P7 = strassen(subtract(A12, A22), sum(B21, B22));
-    
-    vector<vector<int>> C11 = subtract(sum(sum(P1, P4), P5), P7);
+
+    vector<vector<int>> C11 = sum(sum(subtract(P4, P5), P1), P7);
     vector<vector<int>> C12 = sum(P3, P5);
     vector<vector<int>> C21 = sum(P2, P4);
-    vector<vector<int>> C22 = subtract(sum(subtract(P1, P2), P3), P6);
-    
+    vector<vector<int>> C22 = sum(sum(subtract(P3, P2), P1), P6);
+
     return combine(C11, C12, C21, C22);
 }
 
 void printMatrix(const vector<vector<int>>& matrix) {
     int n = matrix.size();
-    
+
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             cout << matrix[i][j] << " ";
@@ -106,19 +106,21 @@ void printMatrix(const vector<vector<int>>& matrix) {
 }
 
 int main() {
-    vector<vector<int>> A = {{1, 2, 3, 4},
-                             {5, 6, 7, 8},
-                             {9, 10, 11, 12},
-                             {13, 14, 15, 16}};
-    
-    vector<vector<int>> B = {{1, 0, 0, 0},
-                             {0, 1, 0, 0},
-                             {0, 0, 1, 0},
-                             {0, 0, 0, 1}};
-    
+    /*     vector<vector<int>> A = {{1, 2, 3, 4},
+                                 {5, 6, 7, 8},
+                                 {9, 10, 11, 12},
+                                 {13, 14, 15, 16}};
+
+        vector<vector<int>> B = {{1, 0, 0, 0},
+                                 {0, 1, 0, 0},
+                                 {0, 0, 1, 0},
+                                 {0, 0, 0, 1}}; */
+
+    vector<vector<int>> A = { {1,2},{3,4} };
+    vector<vector<int>> B = { {1,0},{0,1} };
     vector<vector<int>> result = strassen(A, B);
-    
+
     printMatrix(result);
-    
+
     return 0;
 }
